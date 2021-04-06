eafa3f187d06753498f7fd3a008da0a9afb798c4
Stas Sergeev <stsp@users.sourceforge.net>
2021-04-07 01:02:29 +0300

    increase mem to 2Gb

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 qemu-system-x86_64 -hda buildroot/output/images/disk.img -enable-kvm \
     -cpu host -bios /usr/share/edk2/ovmf/OVMF_CODE.fd \
-    -device intel-hda -device hda-duplex
+    -device intel-hda -device hda-duplex -m 2G
