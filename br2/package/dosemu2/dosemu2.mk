f35d076e02ccb5e8cf83ddc6ad79f405deb50812
Stas Sergeev <stsp@users.sourceforge.net>
2021-04-06 20:17:55 +0300

    enable KVM

--- br2/package/dosemu2/dosemu2.mk
+++ br2/package/dosemu2/dosemu2.mk
@@ -4,8 +4,4 @@ DOSEMU2_USERS = dosemu2 -1 dosemu2 -1 - /home/dosemu2 /bin/bash cdrom,lock,video
 DOSEMU2_AUTORECONF = YES
 DOSEMU2_AUTORECONF_OPTS = -I m4 --install --force
 
-define DOSEMU2_DEVICES
-    /dev/kvm  c  666  root  kvm  10  232  -  -  -
-endef
-
 $(eval $(autotools-package))
--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 qemu-system-x86_64 -hda buildroot/output/images/disk.img -enable-kvm \
-    -bios /usr/share/edk2/ovmf/OVMF_CODE.fd -soundhw hda
+    -cpu host -bios /usr/share/edk2/ovmf/OVMF_CODE.fd \
+    -device intel-hda -device hda-duplex
