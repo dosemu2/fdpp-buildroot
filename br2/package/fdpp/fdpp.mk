72eaaa276ee35ecbcfdc0f747a110b869bfc8e3a
Stas Sergeev <stsp@users.sourceforge.net>
2021-04-06 04:12:21 +0300

    more work on fdpp and dosemu2

---
+++ br2/package/fdpp/fdpp.mk
@@ -0,0 +1,19 @@
+FDPP_VERSION = 1.0
+FDPP_SITE = https://github.com/dosemu2/fdpp
+FDPP_LICENSE = GPL-3.0+
+FDPP_LICENSE_FILES = COPYING
+FDPP_INSTALL_STAGING = YES
+
+define FDPP_BUILD_CMDS
+    cd $(@D) && $$fdpp_SRCDIR/configure && make PREFIX=/usr
+endef
+
+define FDPP_INSTALL_STAGING_CMDS
+    make -C $(@D) install DESTDIR=$(STAGING_DIR) PREFIX=/usr
+endef
+
+define FDPP_INSTALL_TARGET_CMDS
+    make -C $(@D) install DESTDIR=$(TARGET_DIR) PREFIX=/usr
+endef
+
+$(eval $(generic-package))
