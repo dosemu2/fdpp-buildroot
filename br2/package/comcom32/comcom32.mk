12b2f809d30602ed3f842362741cd48f69e554ef
Stas Sergeev <stsp@users.sourceforge.net>
2021-04-08 21:14:49 +0300

    add comcom32 binary

---
+++ br2/package/comcom32/comcom32.mk
@@ -0,0 +1,10 @@
+COMCOM32_VERSION = 0.1
+COMCOM32_SITE = https://github.com/dosemu2/comcom32
+COMCOM32_LICENSE = GPL-3.0+
+COMCOM32_LICENSE_FILES = LICENSE
+
+define COMCOM32_INSTALL_TARGET_CMDS
+    $(MAKE) -C $(@D) install DESTDIR=$(TARGET_DIR) PREFIX=/usr
+endef
+
+$(eval $(generic-package))
