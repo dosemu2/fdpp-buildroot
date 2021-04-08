7c78540d1f30acf6548d45ed74e22ecc1e9aa6ff
Stas Sergeev <stsp@users.sourceforge.net>
2021-04-08 04:22:13 +0300

    fdpp: build fixes

--- br2/package/fdpp/fdpp.mk
+++ br2/package/fdpp/fdpp.mk
@@ -7,15 +7,15 @@ FDPP_INSTALL_STAGING = YES
 FDPP_DEPENDENCIES += elfutils
 
 define FDPP_BUILD_CMDS
-    cd $(@D) && $$fdpp_SRCDIR/configure && make PREFIX=/usr
+    cd $(@D) && $$fdpp_SRCDIR/configure && $(MAKE) PREFIX=/usr CXX=$(TARGET_CXX)
 endef
 
 define FDPP_INSTALL_STAGING_CMDS
-    make -C $(@D) install DESTDIR=$(STAGING_DIR) PREFIX=/usr
+    $(MAKE) -C $(@D) install DESTDIR=$(STAGING_DIR) PREFIX=/usr
 endef
 
 define FDPP_INSTALL_TARGET_CMDS
-    make -C $(@D) install DESTDIR=$(TARGET_DIR) PREFIX=/usr
+    $(MAKE) -C $(@D) install DESTDIR=$(TARGET_DIR) PREFIX=/usr
 endef
 
 $(eval $(generic-package))
