4123d7d67df6d46d457fcdcc86bbdf1bf5eccca2
Stas Sergeev <stsp@users.sourceforge.net>
2021-04-10 16:33:12 +0300

    get comcom32 binary from github

--- br2/package/comcom32/comcom32.mk
+++ br2/package/comcom32/comcom32.mk
@@ -3,6 +3,11 @@ COMCOM32_SITE = https://github.com/dosemu2/comcom32
 COMCOM32_LICENSE = GPL-3.0+
 COMCOM32_LICENSE_FILES = LICENSE
 
+define COMCOM32_BUILD_CMDS
+    cd $(comcom32_SRCDIR) && \
+      curl -O https://dosemu2.github.io/comcom32/files/comcom32.zip && \
+      unzip comcom32.zip
+endef
 define COMCOM32_INSTALL_TARGET_CMDS
     $(MAKE) -C $(@D) install DESTDIR=$(TARGET_DIR) PREFIX=/usr
 endef
