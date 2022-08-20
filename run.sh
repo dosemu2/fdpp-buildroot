5b5ccf4184b03565e1b5883eff75ad7650ae964a
Stas Sergeev <stsp@users.sourceforge.net>
2022-08-20 20:49:39 +0300

    run.sh: add ubuntu ovmf location

--- run.sh
+++ run.sh
@@ -1,3 +1,12 @@
+die() {
+    echo "$1"
+    exit 1
+}
+
+OVMF=/usr/share/edk2/ovmf/OVMF_CODE.fd
+[ -r "$OVMF" ] || OVMF=/usr/share/OVMF/OVMF_CODE.fd
+[ -r "$OVMF" ] || die "ovmf not found"
+
 qemu-system-x86_64 -hda buildroot/output/images/disk.img -enable-kvm \
-    -cpu host -bios /usr/share/edk2/ovmf/OVMF_CODE.fd \
+    -cpu host -bios "$OVMF" \
     -device intel-hda -device hda-duplex -usbdevice tablet -m 2G
