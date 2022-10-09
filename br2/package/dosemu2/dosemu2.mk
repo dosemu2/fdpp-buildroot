5d4e84ee458861f90ddd07303b4c1ebbbe9ce88d
Stas Sergeev <stsp@users.sourceforge.net>
2022-10-09 23:01:08 +0500

    add dosemu2 to kvm group

--- br2/package/dosemu2/dosemu2.mk
+++ br2/package/dosemu2/dosemu2.mk
@@ -1,6 +1,6 @@
 DOSEMU2_VERSION = 2.0
 DOSEMU2_SITE = https://github.com/dosemu2/dosemu2
-DOSEMU2_USERS = dosemu2 -1 dosemu2 -1 - /home/dosemu2 /bin/bash cdrom,lock,video,input,audio dosemu2_user
+DOSEMU2_USERS = dosemu2 -1 dosemu2 -1 - /home/dosemu2 /bin/bash cdrom,lock,video,input,audio,kvm dosemu2_user
 DOSEMU2_AUTORECONF = YES
 DOSEMU2_AUTORECONF_OPTS = -I m4 --install --force
 
