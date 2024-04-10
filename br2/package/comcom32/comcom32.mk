6b780eb08f6a2d89a487cd6dac484d1229c8c601
Stas Sergeev <stsp@users.sourceforge.net>
2024-04-10 21:07:11 +0300

    update

--- br2/package/comcom32/comcom32.mk
+++ br2/package/comcom32/comcom32.mk
@@ -4,7 +4,9 @@ COMCOM32_LICENSE = GPL-3.0+
 COMCOM32_LICENSE_FILES = LICENSE
 
 define COMCOM32_BUILD_CMDS
-    cd $(@D) && make fetch
+    cd $(@D) && \
+      curl -O https://dosemu2.github.io/comcom64/files/comcom32.zip && \
+      unzip -u -o comcom32.zip
 endef
 define COMCOM32_INSTALL_TARGET_CMDS
     $(MAKE) -C $(@D) install DESTDIR=$(TARGET_DIR) PREFIX=/usr
